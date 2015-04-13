ac2c67b49f3970b782968705c8f25819aafa91c7
victo0 <v.loffet@rubika-edu.com>
2015-04-13 17:57:39 +0200

    Nouveau système de vague/spawn d'ennemis

--- Assets/Project assets/WorldObject/Units/Ennemies/Spawner/Spawner.cs
+++ Assets/Project assets/WorldObject/Units/Ennemies/Spawner/Spawner.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using TD;
 
 [System.Serializable]
@@ -16,47 +17,86 @@ public class Spawner : MonoBehaviour
 {
 
     public List<Vagues> wavesList;
+	public float spawnDelay;
+	public float waveDelay; 
+    protected Vagues currentWave;
     private bool onGoing;
     private int activeWave;
-     Unit activeUnit;
+    private Unit activeUnit;
+	private int activeUnitNumber;
+    private float timer;
+	private int waveCount;
+
+    private Vector3 spawnSpot;
     
     private void Start()
     {
-        
+        spawnSpot.x = this.transform.position.x;
+        spawnSpot.y = (this.transform.position.y + 1f);
+        spawnSpot.z = this.transform.position.z;
+		activeWave = 0;
+		onGoing = false;
+		activeUnitNumber = 0;
+		NextWave ();
+		
+		
     }
 
 
     private void Update() 
     {
+        timer += Time.deltaTime;
         if (onGoing)
         {
-            if (activeUnit = null)
+            if (activeUnit != null)
             {
-                waveSpawn();
-
+                WaveSpawn();
             }
             else
             {
                 onGoing = false;
-            }          
-        }	
+				
+            }       
+        }
+		if (activeUnitNumber >= waveCount)
+		{
+			Debug.Log ("Next wave in" + waveDelay + "with timer at" + timer);
+			onGoing = false;
+			if(timer >= waveDelay)
+			{
+				timer = 0;
+				NextWave ();
+			}
+		}  
 	}
 
     public void NextWave()
     {
-        activeWave++;
+		Debug.Log ("Next wave launched");
+		activeUnitNumber = 0;
+		currentWave = wavesList [activeWave];
+		activeUnit = currentWave.ennemis [activeUnitNumber];
+		onGoing = true;
+		activeWave++;
+		waveCount = currentWave.ennemis.Count;
+		if (activeWave >= (wavesList.Count+1))
+		{
+			onGoing = false;
+		}
     }
 
     private void WaveSpawn()
     {
-        foreach (Unit unit in wavesList) 
-        {
-            List<Unit> tempList = new List<Unit> ();
-            tempList = unitList;
-        }
-       
-        
-    }
+        if (timer >= spawnDelay)
+		{
+			timer = 0.0f;
+			activeUnit =  currentWave.ennemis [activeUnitNumber];
+			activeUnitNumber ++;
+			Instantiate(activeUnit, spawnSpot, transform.rotation);
+			waveCount = currentWave.ennemis.Count;
+		}
+		
 
+    }
 
 }
