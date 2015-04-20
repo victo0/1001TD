a1cec92e4613aa4a26588691ffc54432c81c6ae1
Hokakyo <Hokakyo@users.noreply.github.com>
2015-04-20 21:34:18 +0200

    Charmeurs
    Normalement, ça devrait le faire.

---
+++ Assets/Project assets/WorldObject/Projectiles/CharmeursProjectile/CharmeursProjectile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using TD;
+using System.Collections.Generic;
+
+public class ElephantProjectile : Projectile 
+{
+	protected Vector3 positionHit;
+	public float rangeAOE;
+	public List<WorldObject> areaOfEffect;
+	
+	// Ce script contiendrais les différences qu'a ce projectile, étant le projectile de base : il est vide.
+	public override void Update ()
+	{
+		base.Update ();
+	}
+	
+	protected override bool HitSomething ()
+	{
+		return base.HitSomething ();
+	}
+	
+	protected override void InflictDamage ()
+	{
+		//base.InflictDamage ();
+		positionHit = this.transform.position;
+		areaOfEffect = WorkManager.FindNearbyObjects(positionHit, rangeAOE);
+		
+		foreach (WorldObject objet in areaOfEffect)
+		{
+			if (objet.tag == "Ennemi")
+			{
+				objet.TakeDamage(damage);
+			}
+		}
+	}
+}
---
+++ Assets/Project assets/WorldObject/Units/OurUnits/Charmeurs/Charmeurs.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using TD;
+using System.Collections.Generic;
+
+public class Charmeurs : Unit {
+
+	private Quaternion targetRotation;
+	protected Quaternion aimRotation;
+	
+	protected Vector3 positionOfTower;
+	public float areaOfSlow;
+	protected List<WorldObject> slowList;
+	
+	public float speedSlow;
+	public float lengthSlow;
+	
+	protected override void Start () 
+	{
+		base.Start ();
+	}
+	
+	protected override void Update () 
+	{
+		base.Update();
+		if(aiming) 
+		{	 //Gère la rotation de l'unité en direction de l'ennemi lorsqu'il le vise.
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, aimRotation, weaponAimSpeed);
+			CalculateBounds();
+			//sometimes it gets stuck exactly 180 degrees out in the calculation and does nothing, this check fixes that
+			Quaternion inverseAimRotation = new Quaternion(-aimRotation.x, -aimRotation.y, -aimRotation.z, -aimRotation.w);
+			if(transform.rotation == aimRotation || transform.rotation == inverseAimRotation) 
+			{
+				aiming = false;
+			}
+			
+			if (dayNightCycle == true)
+			{
+				positionOfTower = this.transform.position;
+				slowList = WorkManager.FindNearbyObjects(positionOfTower, areaOfSlow);
+				foreach (WorldObject objet in slowList)
+				{
+					if (objet.tag == "Ennemi")
+					{
+						EnnemiUnit betterObjet = objet.GetComponent<EnnemiUnit> ();
+						betterObjet.SlowThatUnit (lengthSlow, speedSlow);
+					}
+				}
+			}
+		}
+	}
+	
+	public override bool CanAttack() 
+	{	 //définis que cette unité peut attaquer.
+		return true;
+	}
+	
+	protected override void AimAtTarget () 
+	{ 	//Définis qu'il doit viser la cible
+		base.AimAtTarget();
+		aimRotation = Quaternion.LookRotation (target.transform.position - transform.position);
+	}
+	
+	protected override void UseWeapon () 
+	{		//Définis ce qu'il se passe lorsqu'on demande à l'unité d'attaquer.
+		base.UseWeapon();
+		
+		if (dayNightCycle == false)
+		{
+			Vector3 spawnPoint = transform.position;
+			spawnPoint.x += (2.1f * transform.forward.x);
+			spawnPoint.y += (2.1F * transform.forward.y);
+			spawnPoint.z += (2.1f * transform.forward.z);
+			GameObject gameObject = (GameObject)Instantiate(ResourceManager.GetWorldObject("CharmeursProjectile"), spawnPoint, transform.rotation);
+			Projectile projectile = gameObject.GetComponentInChildren< Projectile >();
+			projectile.SetRange(0.5f * weaponRange);
+			projectile.SetTarget(target);
+		}
+	}
+	
+	public void StartMove(Vector3 destination) 
+	{	//Lance la rotation (peut etre modifié pour se déplacer vers la cible).
+		this.destination = destination;
+		aimRotation = Quaternion.LookRotation (destination - transform.position);
+		rotating = true;
+		moving = false;
+	}
+}
